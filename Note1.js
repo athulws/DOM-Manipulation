f59a9b45d185ca415e99c8fbce28f776691570f8
athulws <athulraj@weamse.in>
2024-02-06 17:21:49 +0530

    basic colour change

---
+++ Note1.js
@@ -0,0 +1,50 @@
+// ----------------------------------------------Note 1--------------------------------------------------------------------
+
+
+
+//-----------------------------------------------GetElementById()--------------------------------------------------------------------------------------------
+
+// const title = document.getElementById('main-heading') -->  "main-heading" enna class name nte details kittan 
+
+// console.log(title);
+
+//-----------------------------------------------GetElementById()--------------------------------------------------------------------------------------------
+
+
+//-----------------------------------------------GetElementByClassName()------------------------------------------------------------------------------
+    //GetElementByClassName() --> it returns an array like object of all the child elements which have all the given class names
+
+    // const listItems = document.getElementsByClassName('list-items')
+
+    // console.log(listItems);
+//------------------------------------------------GetElementByClassName()-----------------------------------------------------------------------------
+
+//------------------------------------------------getElementByTagName()-----------------------------------------------------------------------------
+
+    //getElementByTagName() --> GetElementByClassName() nte same function aan
+
+    // const listItems = document.getElementsByTagName('li');
+    // console.log(listItems);
+
+//------------------------------------------------getElementByTagName()-----------------------------------------------------------------------------
+
+
+//------------------------------------------------querySelector()-------------------------------------------------------------------------------------------
+// querySelector() --> used to select one item or first item that matches the selector is given
+//                 --> same className, Id, tag okk vannal aadyam koduthathine aan kanikkua
+//                 --> it accepts all css style selectors
+
+    // const container = document.querySelector('div')
+
+    // console.log(container); 
+//------------------------------------------------querySelector()-------------------------------------------------------------------------------------------
+
+//------------------------------------------------querySelectorAll()------------------------------------------------------------------------------------------------------
+// querySelectorAll() --> same class names ulla ellam kandpidikkan
+
+    // const container = document.querySelectorAll('div');
+
+    // console.log(container);
+
+//-------------------------------------------------querySelectorAll(--------------------------------------------------------------------------------------------------------------------------
+
