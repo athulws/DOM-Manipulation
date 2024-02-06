b1f5d675b7f56fdaf5a0ded12c3091c6a782fa47
athulws <athulraj@weamse.in>
2024-02-06 16:43:02 +0530

    basic

---
+++ app.js
@@ -0,0 +1,35 @@
+// GetElementById()
+
+// const title = document.getElementById('main-heading')
+
+// console.log(title);
+
+
+//GetElementByClassName() --> it returns an array like object of all the child elements which have all the given class names
+
+// const listItems = document.getElementsByClassName('list-items')
+
+// console.log(listItems);
+
+
+//getElementByTagName() --> GetElementByClassName() nte same function aan
+
+// const listItems = document.getElementsByTagName('li');
+// console.log(listItems);
+
+
+// querySelector() --> used to select one item or first item that matches the selector is given
+//                 --> same className, Id, tag okk vannal aadyam koduthathine aan kanikkua
+//                 --> it accepts all css style selectors
+
+// const container = document.querySelector('div')
+
+// console.log(container); 
+
+
+// querySelectorAll() --> same class names ulla ellam kandpidikkan
+
+    const container = document.querySelectorAll('div');
+    
+    console.log(container);
+
